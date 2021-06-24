525e3960ae2cacdccc4890cfe6b08894feb27cb6
M. Aidil Rifaldi <muhammadaidilrifaldi@student.untan.ac.id>
2021-06-24 03:31:57 -1200

    UAS PBO - 2021

--- application/Models/TenagaKependidikan.php
+++ application/Models/TenagaKependidikan.php
@@ -1,49 +1,41 @@
 <?php
 
 namespace App\Models;
-
 class TenagaKependidikan extends Pengguna implements Pegawai
 {
     private $nip;
     private $nama;
     private $alamat;
 
-    function __construct($un,$pw,$email,$nip,$nama,$alamat){
+    function __construct($us,$ps,$email,$nip,$nama,$alamat){
         $this->username = $un;
-        $this->password = $pw;
+        $this->password = $ps;
         $this->email = $email;
         $this->nip = $nip;
         $this->nama = $nama;
         $this->alamat = $alamat;
     }
-
     public function hitungTunjanganKinerja()
-    {
-        # code...
+    {    
     }
 
     public function presensiMasuk()
-    {
-        # code...
+    {  
     }
 
     public function setNip($nip)
-    {
-        # code...
+    {    
     }
 
     public function getNip()
-    {
-        # code...
+    { 
     }
 
     public function setNama($nama)
     {
-        # code...
     }
 
     public function getNama()
     {
-        # code...
     }
 }
